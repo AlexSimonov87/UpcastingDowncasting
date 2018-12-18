fe6d805d1b2ce569bec1f735bc9670542901ede9
AlexSimonov87 <alex.simonov87@gmail.com>
2018-12-18 22:16:14 +0300

    Add new Object named "dogee" in class Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,9 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
+
+        Dog dogee = new Dog();
+
         // или  Animal animal = new Dog();
         Dog dog = new Dog();
         // Upcasting - низходящее преобразование
@@ -10,11 +13,10 @@ public class Main {
         // как int помещаяется в long - аналогия
         animal.sleep();
 
-
         // Downcasting - низходящее преобразование. От родителя к Потомку - из Animal в Dog
        //  Dog dog2 = animal; - будет ошибка
 
-// не безопастный способ
+        // не безопастный способ
         Dog dog2 = (Dog)animal;  // в этой переменной animal лежит объект класса DOG
         dog2.bark();
 
